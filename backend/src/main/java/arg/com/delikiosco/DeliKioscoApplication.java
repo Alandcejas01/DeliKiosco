83cf4b6d8d87ac5219a966aec5d48bd7fd8c3bd8
Alan Cejas <cejasalandavid01@gmail.com>
2023-05-16 20:54:14 -0300

    Build(backend): Initial state.

---
+++ backend/src/main/java/arg/com/delikiosco/DeliKioscoApplication.java
@@ -0,0 +1,13 @@
+package arg.com.delikiosco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliKioscoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DeliKioscoApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/arg/com/delikiosco/DeliKioscoApplicationTests.java
@@ -0,0 +1,13 @@
+package arg.com.delikiosco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliKioscoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
